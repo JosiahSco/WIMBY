829950ce84627ac251604b08dab7e68744bb587a
JosiahSco <jscott70@uco.edu>
2023-06-09 09:09:04 -0500

    button now fetches current user lat and long data

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 function buttonAlert() {
-    alert('Weather Got');
+    findMyPosition();
+}
+
+function findMyPosition() {
+    const latitudeElement = document.querySelector("#latitude");
+    const longitudeElement = document.querySelector("#longitude");
+
+    function success(position) {
+        const latitudeNum = position.coords.latitude;
+        const longitudeNum = position.coords.longitude;
+        latitudeElement.textContent = `Latitude: ${latitudeNum}°`;
+        longitudeElement.textContent = `Longitude: ${longitudeNum}°`;
+    }
+
+    function error() {
+        latitude.textContent = "Error getting your location"
+    }
+
+    if (!navigator.geolocation) {
+        latitude.textContent = "Geolocation is not supported by your browser";
+      } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+      }
 }
