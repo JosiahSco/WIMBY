982e4eb00d9362a4e79af8c91d590639d917cf7f
JosiahSco <jscott70@uco.edu>
2023-06-18 17:22:21 -0500

    added new layout after getting location, and forecast placeholders

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ function findMyPosition() {
 
 document.querySelector('#getLocation').addEventListener('click', findMyPosition);
 
+
 document.querySelector('.search-bar').addEventListener('keypress', function(e){
     if (e.keyCode == 13) {
         var hasNumber = /\d/;
@@ -55,47 +56,73 @@ function getCurrentWeather(latitude, longitude) {
     .then(response => response.json())
     .then(json => {
         console.log(json);
-        document.getElementById('currentWeatherList').style.backgroundColor = 'rgba(0,0,0,0.25)';
-        // WEATHER RESULTS
-        // Location
-        document.querySelector('#location').textContent =  json.name;
-        // "AS OF CURRENT TIME"
-        document.querySelector('#datetime').textContent = convertTime(json.dt, true); 
-        // Current Temperature
-        document.querySelector('#temperature').textContent = 'Temperature: ' + json.main.temp + '°F';
-        // Feels Like
-        document.querySelector('#feelslike').textContent = 'Feels Like: ' + json.main.feels_like + '°F'; 
-        // Main Weather Status "rain"
-        // document.querySelector('#conditions').textContent = json.weather[0].main;
-        // Desc for Main Status "moderate rain"
-        document.querySelector('#conditions').textContent = 'Currently ' + json.weather[0].description; 
-        // High Temp 
-        document.querySelector('#tempmax').textContent = 'High: ' + json.main.temp_max + '°F'; 
-        // Low Temp
-        document.querySelector('#tempmin').textContent = 'Low: ' + json.main.temp_min + '°F'; 
-        // Humidity
-        document.querySelector('#humidity').textContent = 'Humidity: ' + json.main.humidity + '%'; 
-        // Wind
-        // - Speed
-        document.querySelector('#windspeed').textContent = 'WIND Speed: ' + json.wind.speed + ' MPH'; 
-        // - Deg 
-        document.querySelector('#winddeg').textContent = 'WIND Deg: ' + json.wind.deg + '°'; 
-        // - Gust
-        document.querySelector('#windgust').textContent = 'WIND Gust: ' + json.wind.gust + ' MPH'; 
-        // Sunset Time
-        document.querySelector('#sunset').textContent = 'Sunset at ' + convertTime(json.sys.sunset, false); 
-        // Sunrise Time
-        document.querySelector('#sunrise').textContent = 'Sunrise at ' + convertTime(json.sys.sunrise, false); 
+        hideSearchElement();
+        document.getElementById('weatherWrapper').style.display = 'flex';
+        fillCurrentWeatherData(json);
+        
     })
 }
 
 function convertTime(timestamp, dow) { // unix timestamp, bool (true if include day of week)
-        var date = new Date(timestamp * 1000); // convert to miliseconds from seconds 
-        const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
-        var day = dayName[date.getDay()]; 
-        var hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours(); // 12-Hour Format 
-        var min = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(); // 10:3 -> 10:03 
-        var period = date.getHours() < 12 ? 'AM' : 'PM'; 
-        var formattedTime = dow ? day + ' ' + hour + ':' + min + ' ' + period : hour + ':' + min + ' ' + period; 
-        return formattedTime; 
+    var date = new Date(timestamp * 1000); // convert to miliseconds from seconds 
+    const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+    var day = dayName[date.getDay()]; 
+    var hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours(); // 12-Hour Format 
+    var min = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes(); // 10:3 -> 10:03 
+    var period = date.getHours() < 12 ? 'AM' : 'PM'; 
+    var formattedTime = dow ? day + ' ' + hour + ':' + min + ' ' + period : hour + ':' + min + ' ' + period; 
+    return formattedTime; 
 } 
+
+document.querySelector('#detailsButton').addEventListener('click', showDetails); 
+
+function displayWeatherIcon(description){
+    switch(description) {
+        //case 'cloudy':
+        //element.src = "graphics/cloudly.png/" or something
+    }
+}
+
+//this is a temporary function for testing purposes
+function hideSearchElement() {
+    document.getElementById('searchLocation').style.display = "none";
+    document.querySelector('.container').style.display = "none";
+}
+
+function showDetails() {
+    document.getElementById('currentWeatherList').style.display === 'none' 
+        ? document.getElementById('currentWeatherList').style.display = 'block' 
+        : document.getElementById('currentWeatherList').style.display = 'none';
+}
+
+function fillCurrentWeatherData(json) {
+    document.getElementById('currentWeatherList').style.backgroundColor = 'rgba(0,0,0,0.25)';
+    // WEATHER RESULTS
+    // Location
+    document.querySelector('#currentLocation').textContent =  json.name;
+    // Current Temperature
+    document.querySelector('#currentTemp').textContent = Math.round(json.main.temp) + '°F';
+    // "AS OF CURRENT TIME"
+    //document.querySelector('#datetime').textContent = convertTime(json.dt, true); 
+    // Feels Like
+    document.querySelector('#feelslike').textContent = 'Feels Like: ' + Math.round(json.main.feels_like) + '°F'; 
+    // Main Weather Status "rain"
+    // document.querySelector('#conditions').textContent = json.weather[0].main;
+    //Desc for Main Status "moderate rain"
+    document.querySelector('#conditions').textContent = 'Currently ' + json.weather[0].description; 
+    // High Temp 
+    document.querySelector('#tempmax').textContent = 'High: ' + Math.round(json.main.temp_max) + '°F'; 
+    // Low Temp
+    document.querySelector('#tempmin').textContent = 'Low: ' + Math.round(json.main.temp_min) + '°F'; 
+    // Humidity
+    document.querySelector('#humidity').textContent = 'Humidity: ' + json.main.humidity + '%'; 
+    // Wind
+    // - Speed
+    document.querySelector('#windspeed').textContent = 'WIND Speed: ' + Math.round(json.wind.speed) + ' MPH'; 
+    // - Deg 
+    document.querySelector('#winddeg').textContent = 'WIND Deg: ' + json.wind.deg + '°'; 
+    // Sunset Time
+    document.querySelector('#sunset').textContent = 'Sunset at ' + convertTime(json.sys.sunset, false); 
+    // Sunrise Time
+    document.querySelector('#sunrise').textContent = 'Sunrise at ' + convertTime(json.sys.sunrise, false); 
+}
